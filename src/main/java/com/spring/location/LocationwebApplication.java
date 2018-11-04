18a7ff16d02e1abd0528173a8c49c57cdc9b72ea
jhansikv <44140817+jhansikv@users.noreply.github.com>
2018-11-04 18:37:17 -0500

    Update LocationwebApplication.java

--- src/main/java/com/spring/location/LocationwebApplication.java
+++ src/main/java/com/spring/location/LocationwebApplication.java
@@ -2,11 +2,19 @@ package com.spring.location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
-public class LocationwebApplication {
+public class LocationwebApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LocationwebApplication.class);
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocationwebApplication.class, args);
